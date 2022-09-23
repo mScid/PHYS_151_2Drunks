bbb77001e17df96d435973b2dcd47df62d933c78
Jan Mariano Dinan Mariano <93442779+mScid@users.noreply.github.com>
2022-09-23 13:59:55 +0800

    Add files via upload

--- drunks_experimental.py
+++ drunks_experimental.py
@@ -3,7 +3,8 @@ from array import *
 from math import factorial
 import matplotlib.pyplot as plt
 
-N = 300
+N = int(input("Enter value of N: "))
+
 
 p = 0.5
 q = 0.5
@@ -19,11 +20,6 @@ while n1 < N+1:
     prob.insert(n1,(factorial(N))/(factorial(n1)*factorial(n2))*(p**n1)*(q**n2))
     n1 += 1
 
-#plt.title("Distribution of the probability of one drunk")
-#plt.xlabel("distance")
-#plt.ylabel("probability")
-#plt.plot(m, prob, color = "blue")
-#plt.show()
 
 prob2 = prob.copy()
 
@@ -33,30 +29,34 @@ while i < N+1:
     prob2[i] = prob[i]**2
     i += 1
 
-#plt.title("Distribution of the probability 2 drunks land on a spot")
-#plt.xlabel("Distance")
-#plt.ylabel("Probability")
-#plt.plot(m, prob2, color = "red")
-#plt.show()
 
 total_probability_together = sum(prob2)
 
 prob3 = prob.copy()
 
-#separation in indices
 separation = 1
 
-prob3.pop()
-i = 0
-while i < N+1 - separation:
-    prob3[i] = 2*prob[i]*prob[i+separation]
-    i += 1
+#separation in indices
 
-print(prob3)
-print(sum(prob3))
+seps = [0]
+sums = [sum(prob2)]
+while separation < N:
+    prob3.pop()
+    i = 0
+    while i < N+1 - separation:
+        prob3[i] = 2*prob[i]*prob[i+separation]
+        i += 1
+    seps.append(separation*2)
+    sums.append(sum(prob3))
+    separation += 1
 
-sums = [sum(prob2),sum(prob3)]
-seps = [0,2]
+print(sum(sums))
+
+#print(prob3)
+#print(sum(prob3))
+
+#sums = [sum(prob2),sum(prob3)]
+#seps = [0,2]
 
 # Initialize the subplot
 figure, axis = plt.subplots(2,2)
@@ -69,10 +69,11 @@ axis [0,0].set_title("Distribution of the probability of one drunk")
 axis [0,1].plot(m, prob2, color = "red")
 axis [0,1].set_title("Distribution of the probability that 2 drunks land on the same spot")
 
-axis [1,0].text(0, 0.5," The total probability that they end up together is " + str(total_probability_together))
+axis [1,1].text(0, 0.5," The total probability that they end up together is " + str(total_probability_together))
+axis [1,1].text(0, 0.8,"Total probability (for checking): " + str(sum(sums)))
 
-axis [1,1].plot(seps, sums)
-axis [1,1].set_title("Plot of probabilities per separation")
+axis [1,0].plot(seps, sums)
+axis [1,0].set_title("Plot of probabilities per separation")
 
 plt.show()
 
